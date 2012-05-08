1c38b4615c787b41e217ea2d225336db92fbe985
Darius Jazayeri <jazayeri@alum.mit.edu>
2012-05-08 15:28:06 -0700

    Initial commit

---
+++ api/src/main/java/org/openmrs/module/distribution/api/DistributionService.java
@@ -0,0 +1,32 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.distribution.api;
+
+import org.openmrs.api.OpenmrsService;
+
+/**
+ * This service exposes module's core functionality. It is a Spring managed bean which is configured in moduleApplicationContext.xml.
+ * <p>
+ * It can be accessed only via Context:<br>
+ * <code>
+ * Context.getService(DistributionService.class).someMethod();
+ * </code>
+ * 
+ * @see org.openmrs.api.context.Context
+ */
+public interface DistributionService extends OpenmrsService {
+
+	// we don't have any service methods yet
+	
+}
